84d16a9b0d1b2974d118d3866403f1e600d913fd
jalugomol2112 <90425998+jalugomol2112@users.noreply.github.com>
2021-10-28 18:48:13 -0500

    Create apps.js

---
+++ interfaz_inicio_registro/apps.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const contraseña = document.getElementById("contraseña")
+const confirmar_contraseña = document.getElementById("confirmar_contraseña")
+const correo_inicio = document.getElementById("correo_inicio")
+const contraseña_inicio = document.getElementById("correo_inicio")
